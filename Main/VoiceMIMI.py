9b6bde969f56fc52e1bdecbff979e580ffab6f33
ztaer <ztaerkiller@gmail.com>
2019-03-26 15:12:29 +0800

    语音播报功能已完毕v2.0更新完毕

--- Main/VoiceMIMI.py
+++ Main/VoiceMIMI.py
@@ -40,27 +40,64 @@ if __name__=="__main__":
     ]
     print("VoiceMIMIv2.0 --- 20190325")
     while True:
+        door = True
         print("1.语音翻译( 支持: 中文 -> 英语 )")
         print("2.文字翻译( 支持: 自动识别 -> 5个语种)")
-        print("3.播报文字( 支持: 中文 - 英文,等待更新困死了 )")
+        print("3.播报文字( 支持: 中文 - 英文)")
+        print("4.语音对话( 将在v3.0更新 )")
         print("q.退出程序")
         choose = input('选择: ')
         if choose == '1':
             times = int(input('请输入录音时间(默认为5秒): '))
             vTplay(times)
+
         elif choose == '2':
             for lang in langEncode:
                 print("-> %d - %s" % (lang[0],lang[2]))
             langNum = int(input("请选择翻译语种: "))
             print("状态: 自动识别 -> %s" % langEncode[langNum][2] )
             inputChosse = input("-> 1 - 直接输入\n-> 2 - 导入文件\n请选择输入方式: ")
-            if inputChosse == '1':
-                strContent = input("请输入: ")
-                print(translation( strContent,langEncode[langNum][1] )+"\n")
-            elif inputChosse == '2':
-                fileRoad = input("请输入文档路径: ")
-                writeStr( fileRoad,langEncode[langNum] )
+            while(door):
+                if inputChosse == '1':
+                    strContent = input("请输入: ")
+                    print(translation( strContent,langEncode[langNum][1] )+"\n")
+                    door = False
+                elif inputChosse == '2':
+                    fileRoad = input("请输入文档路径: ")
+                    writeStr( fileRoad,langEncode[langNum] )
+                    door = False
+                else:
+                    inputChosse = input("输入有误,请重新选择: ")
+                    continue
+
+        elif choose == '3':
+            print("自动识别英语或中文!")
+            inputChosse = input("-> 1 - 直接输入\n-> 2 - 导入文件\n请选择输入方式: ")
+            while(door):
+                if inputChosse == '1':
+                    voiceStrContent = input("请输入: ")
+                    strTo_voice( voiceStrContent )
+                    play_wav( "Play_voice.wav" )
+                    door = False
+                elif inputChosse == '2':
+                    fileRoad = input("请输入文档路径: ")
+                    with open( fileRoad,"rt+",encoding="utf-8" ) as voiceFilse :
+                        strTo_voice( voiceFilse.read() )
+                    print("语音合成完毕!\n音频文件保存到'执行程序目录'名为:'Play_voice.wav'")
+                    playChose = input("是否进行后台播报?(Y/N): ")
+                    if playChose == 'Y' or playChose == 'y':
+                        a = play_wav( "Play_voice.wav" )
+                        print("正在播报!")
+                    else:
+                        print("已取消后台播报！")
+                    door = False
+                else:
+                    inputChosse = input("输入有误,请重新选择: ")
+                    continue
 
         elif choose == 'q':
             print("谢谢使用此程序,下个版本会变得更好!\n作者博客: OO7.fun\n源码链接: https://github.com/ZTaer/VoiceMIMI")
             break
+        else:
+            choose = input("输入有误,请重新选择: ")
+            continue
--- Main/filseRw.py
+++ Main/filseRw.py
@@ -6,14 +6,14 @@ import os
 
 #   写入文件
 #   接受文件路径，语种
-def wFilseName( road,langCn="" ):
+def wFilseName( road,langCn="" ): # 取出路径文件名称,并且去除后缀名
     nameCut = (os.path.basename(road)).split('.')
     return os.path.dirname(road)+os.sep+nameCut[0]+"_"+langCn+".txt"
 
-def  writeStr( road,lang ):
+def  writeStr( road,lang ): # 翻译内容写到文件中
     with open(road, "rt+", encoding="utf-8") as rFilse, open( wFilseName(road,lang[2]),"wt+",encoding="utf-8" ) as wFilse:
         rFilse.seek(0)
-        endStr = translation( rFilse.read().replace('\n','&'),lang[1] ) # 百度翻译遇到‘\n’会停止翻译,并且不会保存空格
+        endStr = translation( rFilse.read().replace('\n','&'),lang[1] ) # 百度翻译遇到‘\n’会停止翻译
         endStr = endStr.replace('&','\n') # 恢复字符串
         wFilse.write(endStr)
 
